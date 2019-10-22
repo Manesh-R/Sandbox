dcaf83668ba0d48942a5d279d482e59b283d514a
Manesh Raveendran <Manesh-R@users.noreply.github.com>
2019-10-21 22:44:58 -0700

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -4,8 +4,17 @@ wget https://raw.githubusercontent.com/Manesh-R/Sandbox/test/setup-cert.sh
 chmod +x setup-cert.sh
 ./setup-cert.sh
 
-pwd
-wget https://raw.githubusercontent.com/Manesh-R/Sandbox/test/setup-nlp.sh
-chmod a+x setup-nlp.sh
-chown demouser.demouser setup-nlp.sh
-sudo -u demouser ./setup-nlp.sh
+sudo -u demouser git clone https://github.com/microsoft/nlp.git /home/demouser/notebooks/nlp
+cd /home/demouser/notebooks/nlp
+sudo -u demouser git checkout origin/ignite
+sudo -u demouser python tools/generate_conda_file.py --gpu
+sudo -u demouser conda env create -n nlp_gpu -f nlp_gpu.yaml
+sudo -u demouser conda activate nlp_gpu
+python -m ipykernel install --user --name nlp_gpu --display-name "nlp_gpu"
+pip install -e .
+python3 setup.py bdist_wheel
+#pwd
+#wget https://raw.githubusercontent.com/Manesh-R/Sandbox/test/setup-nlp.sh
+#chmod a+x setup-nlp.sh
+#chown demouser.demouser setup-nlp.sh
+#sudo -u demouser ./setup-nlp.sh
