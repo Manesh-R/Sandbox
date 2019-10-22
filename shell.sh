7d58547a1335251334722ee2f73e8b8e4fbebe24
Manesh Raveendran <Manesh-R@users.noreply.github.com>
2019-10-21 22:14:59 -0700

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -7,4 +7,5 @@ chmod +x setup-cert.sh
 pwd
 wget https://raw.githubusercontent.com/Manesh-R/Sandbox/test/setup-nlp.sh
 chmod a+x setup-nlp.sh
+chown demouser setup-nlp.sh
 sudo -u demouser ./setup-nlp.sh
