1523c0a0801075736dcc3cc60422de16c13dcc24
Manesh Raveendran <Manesh-R@users.noreply.github.com>
2019-10-22 23:50:27 -0700

    Update setup-nlp.sh

--- setup-nlp.sh
+++ setup-nlp.sh
@@ -4,7 +4,9 @@ git clone https://github.com/microsoft/nlp.git
 cd nlp
 git checkout origin/ignite
 python tools/generate_conda_file.py --gpu
-conda env create -n nlp_gpu -f nlp_gpu.yaml
+export PATH=$PATH:/data/anaconda/envs/py35/bin
+/data/anaconda/envs/py35/bin/conda env create -n nlp_gpu -f nlp_gpu.yaml
+source /anaconda/etc/profile.d/conda.sh
 conda activate nlp_gpu
 python -m ipykernel install --user --name nlp_gpu --display-name "nlp_gpu"
 pip install -e .
